40bf5e8861e4a137216f879e8efce791a9d9fd65
ClausClaus <weixinla@foxmail.com>
2019-12-01 22:52:24 +0800

    添加注释表达管理方式上的不足

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 
+/**
+ *  这种方式在开发中经常被使用，但是如果需求是点击图片也可以发送获取狗狗的图片请求呢！或者其他
+ * 任何地方都可能要触发获取狗狗请求，isLoading这个状态将会越写越多。
+ * 最终导致函数颗粒度过于分散，状态难以集中管理
+ */
+
 const App: React.FC = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [dog, setDog] = useState<null | string>(null);
