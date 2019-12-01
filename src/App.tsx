853e86dc679234242520e15874e1fac3b14a219f
ClausClaus <weixinla@foxmail.com>
2019-12-01 23:26:26 +0800

    增加功能复杂性，状态变得难以管理

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,56 @@ import React, { useState } from 'react';
 const App: React.FC = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [dog, setDog] = useState<null | string>(null);
-    const fetchDog = () => {
+    /**
+     * 增加功能复杂性
+     *  获取狗狗失败，将显示错误
+     *  获取狗狗可以取消
+     */
+    const [canceled, setCanceled] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchRandomDog = () => {
         setIsLoading(true);
-        fetch(`https://dog.ceo/api/breeds/image/random`)
-            .then(data => data.json())
+        return fetch(`https://dog.ceo/api/breeds/image/random`).then(data =>
+            data.json()
+        );
+    };
+
+    const fetchDog = () => {
+        setCanceled(false);
+        setError(null);
+        setIsLoading(true);
+
+        fetchRandomDog()
             .then(response => {
-                setDog(response.message);
+                // 这里的if判断应该可以工作...但是不能！
+                if (canceled) return;
+
                 setIsLoading(false);
+                setDog(response.message);
+            })
+            .catch(error => {
+                setIsLoading(false);
+                setCanceled(false);
+                setError(error);
             });
     };
+
+    const cancel = () => {
+        setIsLoading(false);
+        setCanceled(true);
+    };
+
     return (
         <div>
+            {error && <span style={{ color: 'red' }}>{error}</span>}
             <figure className="dog" onDoubleClick={fetchDog}>
                 {dog && <img src={dog} alt="doggo" />}
             </figure>
             <button disabled={isLoading} onClick={fetchDog}>
                 {isLoading ? 'Fetching...' : 'Fetch dog!'}
             </button>
+            <button onClick={cancel}>Cancel</button>
         </div>
     );
 };
