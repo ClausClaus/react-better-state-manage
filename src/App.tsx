2d7e92bc934dee14a9e381e69bafcdc1c711f262
ClausClaus <weixinla@foxmail.com>
2019-12-02 00:11:16 +0800

    将状态交由reducer进行管理，使用dispatch改变状态，简化页面逻辑

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,49 @@
-import React, { useState } from 'react';
-
-/**
- *  这种方式在开发中经常被使用，但是如果需求是点击图片也可以发送获取狗狗的图片请求呢！或者其他
- * 任何地方都可能要触发获取狗狗请求，isLoading这个状态将会越写越多。
- * 最终导致函数颗粒度过于分散，状态难以集中管理
- */
+import React, { useReducer, useEffect } from 'react';
+import { dogReducer, initialState } from './state';
 
 const App: React.FC = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [dog, setDog] = useState<null | string>(null);
-    /**
-     * 增加功能复杂性
-     *  获取狗狗失败，将显示错误
-     *  获取狗狗可以取消
-     */
-    const [canceled, setCanceled] = useState(false);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(dogReducer, initialState);
+    const { error, dog, status } = state;
 
-    const fetchRandomDog = () => {
-        setIsLoading(true);
-        return fetch(`https://dog.ceo/api/breeds/image/random`).then(data =>
+    useEffect(() => {
+        if (state.status === 'loading') {
+            let canceled = false;
+
+            fetchRandomDog()
+                .then(data => {
+                    if (canceled) return;
+                    dispatch({ type: 'RESOLVE', data });
+                })
+                .catch(error => {
+                    if (canceled) return;
+                    dispatch({ type: 'REJECT', error });
+                });
+
+            return () => {
+                canceled = true;
+            };
+        }
+    }, [state.status]);
+
+    const fetchRandomDog = () =>
+        fetch(`https://dog.ceo/api/breeds/image/random`).then(data =>
             data.json()
         );
-    };
-
-    const fetchDog = () => {
-        setCanceled(false);
-        setError(null);
-        setIsLoading(true);
-
-        fetchRandomDog()
-            .then(response => {
-                // 这里的if判断应该可以工作...但是不能！
-                if (canceled) return;
-
-                setIsLoading(false);
-                setDog(response.message);
-            })
-            .catch(error => {
-                setIsLoading(false);
-                setCanceled(false);
-                setError(error);
-            });
-    };
-
-    const cancel = () => {
-        setIsLoading(false);
-        setCanceled(true);
-    };
 
     return (
         <div>
             {error && <span style={{ color: 'red' }}>{error}</span>}
-            <figure className="dog" onDoubleClick={fetchDog}>
+            <figure
+                className="dog"
+                onDoubleClick={() => dispatch({ type: 'FETCH' })}
+            >
                 {dog && <img src={dog} alt="doggo" />}
             </figure>
-            <button disabled={isLoading} onClick={fetchDog}>
-                {isLoading ? 'Fetching...' : 'Fetch dog!'}
+
+            <button onClick={() => dispatch({ type: 'FETCH' })}>
+                {status === 'loading' ? 'Fetching...' : 'Fetch dog!'}
             </button>
-            <button onClick={cancel}>Cancel</button>
+            <button onClick={() => dispatch({ type: 'CANCEL' })}>Cancel</button>
         </div>
     );
 };
---
+++ src/state.ts
@@ -0,0 +1,34 @@
+export function dogReducer(state: any, event: any) {
+    switch (event.type) {
+        case 'FETCH':
+            return {
+                ...state,
+                status: 'loading'
+            };
+        case 'RESOLVE':
+            return {
+                ...state,
+                status: 'success',
+                dog: event.data.message
+            };
+        case 'REJECT':
+            return {
+                ...state,
+                status: 'failure',
+                error: event.error
+            };
+        case 'CANCEL':
+            return {
+                ...state,
+                status: 'idle'
+            };
+        default:
+            return state;
+    }
+}
+
+export const initialState = {
+    status: 'idle',
+    dog: null,
+    error: null
+};
